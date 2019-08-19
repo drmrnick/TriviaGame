931cd0dc73b47d5fafdcc1c25c65404a7bd9ae56
Nick Tuttle <drmrnick87@gmail.com>
2019-08-19 00:26:49 -0400

    start button
    made the start button remove

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,17 @@
 $('#start').on('click',function(){
-    for(var i=0;i<questions.length;1++);{
-        $('#wrapper2').append('<h2>'+questions[i].questions+'</h2');
-        for(var a=0;a<questions[i].answers.length;a++){
-            
+    $('#start').remove();
+    for(var i=0;i<questions.length;i++){
+        $('#subwrapper').append('<h2>'+questions[i].questions+'</h2');
+        for(var j=0;j<questions[i].answers.length;j++){
+        $('#subwrapper').append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
         }
     }
 })
 
 
-var question =[{
-    questions:"What was the first computer that Apple made?",
-    answers:["iMac", "1-Apple","Apple-1","Macintosh"],
-    correctAnswers:"Apple-1"
+var questions = [{
+    questions: "What was the first computer that Apple made?",
+    answers: ["iMac", "1-Apple","Apple-1","Macintosh"],
+    correctAnswers: "Apple-1"
 }]
+
