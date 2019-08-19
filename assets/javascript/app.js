d6554530a14925d355c2162b13952e1637bde157
Nick Tuttle <drmrnick87@gmail.com>
2019-08-18 23:46:03 -0400

    question making
    tryingt to wire up the appends and get the questions displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
 $('#start').on('click',function(){
-    console.log("Clicked")
+    for(var i=0;i<questions.length;1++);{
+        $('#wrapper2').append('<h2>'+questions[i].questions+'</h2');
+        for(var a=0;a<questions[i].answers.length;a++){
+            
+        }
+    }
 })
+
+
+var question =[{
+    questions:"What was the first computer that Apple made?",
+    answers:["iMac", "1-Apple","Apple-1","Macintosh"],
+    correctAnswers:"Apple-1"
+}]
