0e8771fac7b3abafc739d15f35b419c16a48a0ae
Nick Tuttle <drmrnick87@gmail.com>
2019-08-19 00:36:04 -0400

    trying to add timer
    adding the timer and trying to get a countdown to start up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,34 @@
-$('#start').on('click',function(){
-    $('#start').remove();
-    for(var i=0;i<questions.length;i++){
-        $('#subwrapper').append('<h2>'+questions[i].questions+'</h2');
-        for(var j=0;j<questions[i].answers.length;j++){
-        $('#subwrapper').append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
-        }
-    }
+$('#start').on('click', function () {
+    game.start
 })
 
 
 var questions = [{
     questions: "What was the first computer that Apple made?",
-    answers: ["iMac", "1-Apple","Apple-1","Macintosh"],
+    answers: ["iMac", "1-Apple", "Apple-1", "Macintosh"],
     correctAnswers: "Apple-1"
 }]
 
+var answers = {
+    correct: 0,
+    incorrect: 0,
+    timeCount: 60,
+    timer: function () {
+        game.timeCount--;
+        $('#timer').html(game.timeCount);
+        if (game.timeCount <= 0) {
+            console.log("Game over loser");
+         game.over();
+        }
+    }
+    start:function(){
+        timer = setInterval(game.timeCount,1000);
+        $('#start').remove();
+        for (var i = 0; i < questions.length; i++) {
+            $('#subwrapper').append('<h2>' + questions[i].questions + '</h2');
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                $('#subwrapper').append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+            }
+        }
+    }
+}
